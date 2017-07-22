f3f250054ee3e71ade40de86f5b56bbc56e1a6ba
Durgaprasad <durgaprasad.budhwani@gmail.com>
2017-07-22 18:01:14 +0530

    Added example to get policy files from Intune Application Protection Configuration

--- example/index.ios.js
+++ example/index.ios.js
@@ -13,15 +13,76 @@ import {
   View,
   Button,
 } from 'react-native';
-import RNReactNativeMsIntuneMam from 'react-native-ms-intune-mam'
+import RNReactNativeMsIntuneMam from 'react-native-ms-intune-mam';
+import AzureAdal from 'react-native-azure-adal';
+
+const authority = "https://login.windows.net/common/oauth2/authorize"; //"https://login.windows.net/ariaserver.onmicrosoft.com";
+const resourceUri =  "https://graph.windows.net/"; //"https://ariamobileappproxy-ariaserver.msappproxy.net/VMS.ARIAMobile.DummryService/";
+
+const clientId = "6c7e8096-f593-4d72-807f-a5f86dcc9c77"; //'f00ae3c0-172f-4978-a409-7d7bb29b1026';
+
+const redirectUri = "urn:ietf:wg:oauth:2.0:oob"; //"http://TodoListClient"; //"x-msauth-awesomeproject://com.varian.awesomeproject"; //
 
 export default class example extends Component {
-  _onLoginPress () {
-  
+  async _onLoginPress () {
+    try {
+      let isConfigure =  await AzureAdal.configure(authority, false, clientId, redirectUri, false);
+      console.log(isConfigure);
+      // let userId = await RNReactNativeMsIntuneMam.getCurrentEnrolledAccount();
+      let result = await AzureAdal.loginWithPrompt(resourceUri);
+      console.log(result);
+      let enrolled1 = await RNReactNativeMsIntuneMam.registerAndEnrollAccount(result.userInfo.displayableId, false);
+      console.log(enrolled1);
+    }
+    catch (error) {
+      console.log(error);
+    }
   }
   
-  _logout () {
   
+  
+  async _logout () {
+    await AzureAdal.logout(resourceUri);
+    let user = await RNReactNativeMsIntuneMam.getCurrentEnrolledAccount();
+    console.log('user', user);
+    if(user){
+      let enrolled = await RNReactNativeMsIntuneMam.deRegisterAndUnenrollAccount(user, false);
+      console.log(enrolled);
+    }
+  }
+  
+  async _loginWithApplicationUrl() {
+    let isConfigure =  await AzureAdal.configure(authority, false, clientId, redirectUri, false);
+    let user = await AzureAdal.login(resourceUri);
+    let redirectUri = "http://ariamobileapp-redirect-uri";
+    let configurations = await RNReactNativeMsIntuneMam.getAppConfiguration(null);
+    if(configurations){
+      // console.log(result);
+      console.log(user);
+      
+      try{
+	let config = configurations[0];
+	let clientId = config.applicationId;
+	let applicationUrl = config.applicationUrl;
+	let authority = `https://login.windows.net/${user.tenantId}`;
+	console.log(clientId);
+	console.log(applicationUrl);
+	console.log(authority);
+	await AzureAdal.configure(authority, false, clientId, redirectUri, false);
+	let result = await AzureAdal.login(applicationUrl, user.userInfo.displayableId);
+	console.log(result);
+      }
+      catch(error){
+        console.log(error);
+      }
+     
+	
+    }
+   
+    // console.log(isConfigure);
+    // // let userId = await RNReactNativeMsIntuneMam.getCurrentEnrolledAccount();
+    // let result = await AzureAdal.loginWithPrompt(resourceUri);
+    
   }
   
   async _getConfiguration () {
@@ -42,6 +103,12 @@ export default class example extends Component {
 	  <Button
 	    onPress={this._onLoginPress.bind(this)}
 	    title="Login"
+	  />
+	</View>
+	<View style={{marginBottom:10}}>
+	  <Button
+	    onPress={this._loginWithApplicationUrl.bind(this)}
+	    title="Login With Application Data"
 	  />
 	</View>
 	<View style={{marginBottom:10}}>
--- ios/RNReactNativeMsIntuneMam.m
+++ ios/RNReactNativeMsIntuneMam.m
@@ -10,6 +10,22 @@
 }
 RCT_EXPORT_MODULE()
 
+
+/**
+ *  This method will remove the provided account from the list of
+ *  registered accounts.  Once removed, if the account has enrolled
+ *  the application, the account will be un-enrolled.
+ *
+ *  @note In the case where an un-enroll is initiated, this method will block
+ *  until the MAM token is acquired, then return.  This method must be called before
+ *  the user is removed from the application (so that required AAD tokens are not purged
+ *  before this method is called).xx
+ *
+ *  @param identity The UPN of the account to be removed.
+ *  @param doWipe   If YES, a selective wipe if the account is un-enrolled
+ *  @param resolve - if request is successful
+ *  @param reject - if request is failed
+ */
 RCT_REMAP_METHOD(deRegisterAndUnenrollAccount,
                  identity:(NSString *)identity
                  withWipe:(BOOL)doWipe
@@ -17,15 +33,31 @@ RCT_REMAP_METHOD(deRegisterAndUnenrollAccount,
                  rejecter:(RCTPromiseRejectBlock)reject ){
     @try{
         IntuneMAMEnrollmentManager* intuneMAMEnrollmentManager = [IntuneMAMEnrollmentManager instance];
-        [intuneMAMEnrollmentManager deRegisterAndUnenrollAccount:identity withWipe:NO];
-
+        [intuneMAMEnrollmentManager deRegisterAndUnenrollAccount:identity withWipe:doWipe];
+        
         resolve( @"success" );
     }
     @catch(NSError *error){
         reject( [[NSString alloc] initWithFormat:@"%d", error.code], error.localizedDescription, error );
     }
-
+    
 }
+
+/**
+ *  This method will add the account to the list of registered accounts.
+ *  An enrollment request will immediately be started.  If the enrollment
+ *  is not successful, the SDK will periodically re-try the enrollment every
+ *  24 hours.
+ *  If the application has already registered an account using this API, and calls
+ *  it again, the SDK will ignore the request and output a warning.
+ *
+ *  @param identity The UPN of the account to be registered with the SDK
+ *  @param forceLogin -  if true - Creates an enrollment request which is started immediately.
+ *                       The user will be prompted to enter their credentials,
+ *                       and we will attempt to enroll the user.
+ *  @param resolve - if request is successful
+ *  @param reject - if request is failed
+ */
 
 RCT_REMAP_METHOD(registerAndEnrollAccount,
                  identity:(NSString *)identity
@@ -40,15 +72,29 @@ RCT_REMAP_METHOD(registerAndEnrollAccount,
         else{
             [intuneMAMEnrollmentManager registerAndEnrollAccount:identity];
         }
-
         resolve( @"success" );
+        IntuneMAMPolicyManager* policyManager = [IntuneMAMPolicyManager instance];
+        NSString* uiIdentity = [policyManager getUIPolicyIdentity];
+        if(uiIdentity != identity){
+            [policyManager setUIPolicyIdentity:identity
+                             completionHandler:^(IntuneMAMSwitchIdentityResult result) {
+                                 resolve( @"success" );
+                             }];
+        }
+        else{
+            resolve( @"success" );
+        }
     }
     @catch(NSError *error){
         reject( [[NSString alloc] initWithFormat:@"%d", error.code], error.localizedDescription, error );
     }
 }
 
-
+/**
+ *  Returns a list of UPNs of account currently registered with the SDK.
+ *
+ *  @return Array containing UPNs of registered accounts
+ */
 RCT_REMAP_METHOD(getRegisteredAccounts,
                  resolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject ){
@@ -62,6 +108,12 @@ RCT_REMAP_METHOD(getRegisteredAccounts,
     }
 }
 
+/**
+ *  Returns the UPN of the currently enrolled user.  Returns
+ *  nil if the application is not currently enrolled.
+ *
+ *  @return UPN of the enrolled account
+ */
 RCT_REMAP_METHOD(getCurrentEnrolledAccount,
                  resolve:(RCTPromiseResolveBlock)resolve
                  reject:(RCTPromiseRejectBlock)reject ){
@@ -75,6 +127,17 @@ RCT_REMAP_METHOD(getCurrentEnrolledAccount,
     }
 }
 
+/**
+ *  An array of Dictionaries containing all the AppConfig data sent
+ *  by the MAM service.  If a configuration Dictionary was sent as
+ *  a tenant wide default configuration, the __IsDefault key will be
+ *  present in that dictionary and the value for that key will be set
+ *  to true.  The __IsDefault key will only be present in the Dictionary
+ *  representing the tenant wide default configuration.  All other
+ *  configuration dictionaries will contain targeted policies, and
+ *  these targeted App Configuration settings should always take
+ *  precedence over the tenant wide default configuration settings.
+ */
 RCT_REMAP_METHOD(getAppConfiguration,
                  identity:(NSString *)identity
                  resolver:(RCTPromiseResolveBlock)resolve
